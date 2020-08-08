54d6eba8f84220ece9381d0c919fb748c038801a
emiliocm9 <emilio.contreras97@gmail.com>
2020-08-08 01:26:13 -0500

    CREATE design login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to edit_user_path(@user), notice: 'Logged In'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logged Out!'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :sessions
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
   resources :users
   root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
